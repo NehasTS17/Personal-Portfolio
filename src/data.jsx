25e6f0a68c43077eef0bfa99c4ef8632b3d92d96
Nehas TS <nehas170499@gmail.com>
2024-06-17 22:06:36 +0530

    Update data.jsx

--- src/data.jsx
+++ src/data.jsx
@@ -326,7 +326,7 @@ export const portfolio = [
       {
         icon: <FiExternalLink />,
         title: 'Preview : ',
-        desc: 'https://nehas-Portfolio.netlify.app/',
+        desc: 'https://nehas-portfolio.netlify.app/',
       },
     ],
   },
